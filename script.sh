6cc0fa52fafbbe00752eafa45ab6f0e4a239cf7b
set <serhii.dovhan@gmail.com>
2018-05-09 23:18:36 +0300

    5

--- script.sh
+++ script.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 sudo cp /tmp/grafana.repo /etc/yum.repos.d/grafana.repo
-# sudo rpm --import https://grafanarel.s3.amazonaws.com/RPM-GPG-KEY-grafana
 sudo yum -y update
 sudo yum -y install grafana vim telnet nmap
 
